44e1246d9f3831981a1e6c1c55a814d1793be412
dd-jiyun <dlwldbs8015@gmail.com>
2025-07-14 23:28:43 +0900

    [Baekjoon] 11659 - 구간 합 구하기 4

---
+++ 이지윤/b11659.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class b11659 {
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int[] sum = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            bw.write((sum[j] - sum[i - 1]) + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
