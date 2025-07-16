c6e6326b082f609e2f5cb873a423375a992661cd
sungw0o <sungwoo515@gmail.com>
2025-07-16 16:55:48 +0900

    [BOJ] 2164 카드합

---
+++ 장성우/n2164.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class n2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> cards = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            cards.addLast(i);
+        }
+
+        while(cards.size() > 1) {
+            cards.removeFirst();
+            int temp = cards.removeFirst();
+            cards.addLast(temp);
+        }
+
+        System.out.println(cards.removeFirst());
+        br.close();
+    }
+}
