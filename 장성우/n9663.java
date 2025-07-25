df3dd0227339942513d20b3443fd2311e52ab364
sungw0o <sungwoo515@gmail.com>
2025-07-25 21:22:08 +0900

    [BOJ] 9663 N-QUEEN

---
+++ 장성우/n9663.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n9663 {
+    private static int[] queens;
+    private static int count = 0;
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        queens = new int[N];
+        dfs(0);
+        System.out.println(count);
+        br.close();
+    }
+
+    static void dfs(int row) {
+        if (row == N) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < N; col++) {
+            if (isSafe(row, col)) {
+                queens[row] = col;
+                dfs(row + 1);
+            }
+        }
+    }
+
+    static boolean isSafe(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (queens[i] == col) return false;
+
+            if (Math.abs(row - i) == Math.abs(col - queens[i])) return false;
+        }
+        return true;
+    }
+}
