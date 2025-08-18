b534ee623f1dfb8cac5a21c0333cb80ed6b2ceee
dd-jiyun <dlwldbs8015@gmail.com>
2025-08-18 15:21:50 +0900

    [Beakjoon] 2775 - 부녀회장이 될테야(조합)

--- 이지윤/B2775.java
+++ 이지윤/B2775.java
@@ -1,35 +1,29 @@
 package 이지윤;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class B2775 {
+    static long comb(int n, int r) {
+        if (r == 0 || r == n) {
+            return 1;
+        }
+        long result = 1;
+        for (int i = 1; i <= r; i++) {
+            result = result * (n - i + 1) / i;
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int[][] dp = new int[15][15];
-
-        for (int i = 1; i <= 14; i++) {
-            dp[0][i] = i;
-        }
-
-        for (int k = 1; k <= 14; k++) {
-            for (int n = 1; n <= 14; n++) {
-                if (n == 1) {
-                    dp[k][n] = 1;
-                } else {
-                    dp[k][n] = dp[k][n - 1] + dp[k - 1][n];
-                }
-            }
-        }
 
         int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-            sb.append(dp[k][n]).append("\n");
+
+            sb.append(comb(n + k, k + 1)).append("\n");
         }
 
         System.out.print(sb);
