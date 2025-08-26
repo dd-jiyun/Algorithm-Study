904cb00e7e82a9effe902d7be7795a96f17e9f73
dd-jiyun <dlwldbs8015@gmail.com>
2025-08-26 23:08:57 +0900

    [Beakjoon] 9252 - LCS2

---
+++ 이지윤/B9252.java
@@ -0,0 +1,48 @@
+package 이지윤;
+
+import java.io.*;
+
+public class B9252 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String B = br.readLine();
+
+        int n = A.length();
+        int m = B.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][m]);
+
+        StringBuilder sb = new StringBuilder();
+        int i = n, j = m;
+
+        while (i > 0 && j > 0) {
+            if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                sb.append(A.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    i--;
+                } else {
+                    j--;
+                }
+            }
+        }
+
+        System.out.println(sb.reverse());
+        br.close();
+    }
+}
