42acb1e60a47db392856076183cdee6afe83e01e
sungw0o <sungwoo515@gmail.com>
2025-09-02 17:18:04 +0900

    [프로그래머스] 의상

---
+++ 장성우/p42578.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class p42578 {
+    class Solution {
+        public int solution(String[][] clothes) {
+            Map<String, Integer> map = new HashMap<>();
+            int answer = 1;
+
+            for(int i = 0; i < clothes.length; i++) {
+
+                map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+            }
+
+            for(String key : map.keySet()) {
+                answer *= (map.get(key) + 1);
+            }
+
+            answer -= 1;
+
+            return answer;
+        }
+    }
+}
