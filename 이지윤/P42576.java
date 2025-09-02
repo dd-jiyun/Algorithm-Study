c4b5fd29ff0f85c56fa6dbe5f5a3e2820d9a4cac
dd-jiyun <dlwldbs8015@gmail.com>
2025-09-02 16:29:34 +0900

    [Programmers] 프로그래머스 - 완주하지 못한 선수(정렬)

---
+++ 이지윤/P42576.java
@@ -0,0 +1,31 @@
+package 이지윤;
+
+import java.util.Arrays;
+
+public class P42576 {
+    static class Solution {
+        public String solution(String[] participant, String[] completion) {
+            Arrays.sort(participant);
+            Arrays.sort(completion);
+
+            for (int i = 0; i < completion.length; i++) {
+                if (!participant[i].equals(completion[i])) {
+                    return participant[i];
+                }
+            }
+
+            return participant[participant.length - 1];
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion = {"josipa", "filipa", "marina", "nikola"};
+        System.out.println(solution.solution(participant, completion)); // vinko
+
+        String[] participant2 = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion2 = {"stanko", "ana", "mislav"};
+        System.out.println(solution.solution(participant2, completion2)); // mislav
+    }
+}
